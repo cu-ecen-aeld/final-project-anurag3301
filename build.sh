bf19a942456c739c41ecf10978f8af414596853a
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2025-09-01 12:08:17 +0530

    Added build script to start the build for base yocto image raspberry pi: fixes #1

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+source poky/oe-init-build-env
+
+LAYERS_DIR=$(pwd)/..
+
+if ! grep -q "meta-raspberrypi" conf/bblayers.conf; then
+    bitbake-layers add-layer "$LAYERS_DIR/meta-raspberrypi"
+fi
+
+for layer in meta-oe meta-python meta-networking; do
+    if ! grep -q "$layer" conf/bblayers.conf; then
+        bitbake-layers add-layer "$LAYERS_DIR/meta-openembedded/$layer"
+    fi
+done
+
+if grep -q '^MACHINE ?=' conf/local.conf; then
+    sed -i 's/^MACHINE ?=.*/MACHINE ?= "raspberrypi4-64"/' conf/local.conf
+elif grep -q '^MACHINE =' conf/local.conf; then
+    sed -i 's/^MACHINE =.*/MACHINE ?= "raspberrypi4-64"/' conf/local.conf
+else
+    echo 'MACHINE ?= "raspberrypi4-64"' >> conf/local.conf
+fi
+
+echo "Layers added and MACHINE set to raspberrypi4-64"
+echo " -------- Building core-image-minimal ----------"
+
+bitbake core-image-minimal
