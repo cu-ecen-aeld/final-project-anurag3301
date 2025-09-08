51b6712fcbf7526b4c8e679ba8e3b9994e74507b
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2025-09-08 11:17:08 +0530

    Fixes #6: Completed hardware interaction with passkey auth

--- lockpi/main.c
+++ lockpi/main.c
@@ -68,12 +68,63 @@ char* checkCredentials(char* passkey){
     return NULL;
 }
 
+void display_lcd(char* buf){
+    FILE *file = fopen("/dev/lcd1602", "w");
+    if (file == NULL) {
+        printf("/dev/lcd1602 not found!, maybe driver is not loaded\n");
+        return;
+    }
+    fprintf(file, "%s", buf);
+    fclose(file);
+}
+
+void door_control(char c){
+    FILE *file = fopen("/dev/doorlock", "w");
+    if (file == NULL) {
+        printf("/dev/doorlock not found!, maybe driver is not loaded\n");
+        return;
+    }
+    fprintf(file, "%c\n", c);
+    fclose(file);
+}
+
 int main(){
     init_list();
-    // int fd = open("/dev/keypad", O_RDONLY);
-    // char c;
-    // while (read(fd, &c, 1) > 0) {
-    //     printf("Got char: %c\n", c);
-    // }
+    char passkey_buf[7] = {0};
+    int buf_pos = 0;
+    char lcd_buf[33];
+    int fd = open("/dev/keypad", O_RDONLY);
+
+    snprintf(lcd_buf, 32, "Enter Passkey\n%s", passkey_buf);
+    display_lcd(lcd_buf);
+    door_control('0');
+
+    char c;
+    while (read(fd, &c, 1) > 0) {
+        passkey_buf[buf_pos++] = c;
+        snprintf(lcd_buf, 32, "Enter Passkey\n%s", passkey_buf);
+        display_lcd(lcd_buf);
+        if(buf_pos == 6){
+            printf("\nGOT PASSKEY: %s\n", passkey_buf);
+            char* name = checkCredentials(passkey_buf);
+            if(name == NULL){
+                printf("Invalid Credentials\n\n");
+                snprintf(lcd_buf, 32, "Invalid Passkey\nTry Again");
+                display_lcd(lcd_buf);
+            }
+            else{
+                printf("Welcome %s\n\n", name);
+                snprintf(lcd_buf, 32, "Welcome %s\nOpening Doors", name);
+                display_lcd(lcd_buf);
+                door_control('1');
+            }
+            buf_pos = 0;
+            memset(passkey_buf, '\0', 7);
+            sleep(5);
+            snprintf(lcd_buf, 32, "Enter Passkey\n%s", passkey_buf);
+            display_lcd(lcd_buf);
+            door_control('0');
+        }
+    }
     free_list();
 }
